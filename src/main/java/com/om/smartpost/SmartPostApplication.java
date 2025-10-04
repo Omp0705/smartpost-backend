f3b8c017d7b43a6c3f8ff7e2cae02696902c2060
Om Patil <patilom4634@gmail.com>
2025-10-04 21:28:23 +0530

    initial-commit

---
+++ src/main/java/com/om/smartpost/SmartPostApplication.java
@@ -0,0 +1,13 @@
+package com.om.smartpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPostApplication.class, args);
+	}
+
+}
